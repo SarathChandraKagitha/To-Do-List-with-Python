27506e5932879106d243b6899e04e9e3c4a024cb
Sarath Chandra <kagithasarathchandra@gmail.com>
2023-12-01 23:19:37 +0530

    Create To-Do List Application.py

---
+++ To-Do List Application.py
@@ -0,0 +1,30 @@
+class TodoList:
+    def __init__(self):
+        self.tasks = []
+
+    def add_task(self, task):
+        self.tasks.append(task)
+        print(f"Task '{task}' added to the to-do list.")
+
+    def view_tasks(self):
+        if not self.tasks:
+            print("No tasks in the to-do list.")
+        else:
+            print("To-Do List:")
+            for index, task in enumerate(self.tasks, start=1):
+                print(f"{index}. {task}")
+
+    def remove_task(self, task):
+        if task in self.tasks:
+            self.tasks.remove(task)
+            print(f"Task '{task}' removed from the to-do list.")
+        else:
+            print(f"Task '{task}' not found in the to-do list.")
+
+# Example usage
+todo_list = TodoList()
+todo_list.add_task("Finish coding assignment")
+todo_list.add_task("Buy groceries")
+todo_list.view_tasks()
+todo_list.remove_task("Buy groceries")
+todo_list.view_tasks()
